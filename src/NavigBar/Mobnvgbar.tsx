5a2aaee445cd2d1e9f3fa7e0a3f75a4bc8284df7
Suraj S <suraj_b170389ec@nitc.ac.in>
2021-01-19 16:52:34 +0530

    Icon click to home page

--- src/NavigBar/Mobnvgbar.tsx
+++ src/NavigBar/Mobnvgbar.tsx
@@ -46,11 +46,11 @@ export default function MobileNavBar() {
       <AppBar position="static">
         <Toolbar>
           <div>
-            <img
+            <a href="/" ><img
               className={classes.logo}
               src="https://upload.wikimedia.org/wikipedia/en/d/d0/National_Institute_of_Technology%2C_Calicut_Logo.png"
               alt="Nitc logo"
-            />
+            /></a>
           </div>
           <div className={classes.groupa}>
             <a href="/">
--- src/NavigBar/Nvgbar.tsx
+++ src/NavigBar/Nvgbar.tsx
@@ -60,12 +60,12 @@ export default function PrimaryNavBar() {
     <div>
       <AppBar position="static">
         <Toolbar>
-          <div>
+          <div><a href="/">
             <img
               className={classes.logo}
               src="https://upload.wikimedia.org/wikipedia/en/d/d0/National_Institute_of_Technology%2C_Calicut_Logo.png"
               alt="NITC logo"
-            />
+            /></a>
           </div>
           <div className={classes.groupa}>
             <a href="/">
